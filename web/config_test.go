9a81aef57ffdf58a7aaa706619ae3b50016b2138
mat285 <mat285@cornell.edu>
2019-05-09 16:55:56 -0400

    config tests

--- web/config_test.go
+++ web/config_test.go
@@ -1,1 +1,40 @@
 package web
+
+import (
+	"testing"
+
+	"github.com/blend/go-sdk/webutil"
+
+	"github.com/blend/go-sdk/assert"
+)
+
+func TestConfigBindAddrOrDefault(t *testing.T) {
+	assert := assert.New(t)
+	c := &Config{}
+	assert.Equal(DefaultBindAddr, c.BindAddrOrDefault())
+	assert.Equal("localhost:10", c.BindAddrOrDefault("localhost:10"))
+	c.Port = 10
+	assert.Equal(":10", c.BindAddrOrDefault())
+	c.BindAddr = "localhost:10"
+	assert.Equal(c.BindAddr, c.BindAddrOrDefault())
+}
+
+func TestConfigPortOrDefault(t *testing.T) {
+	assert := assert.New(t)
+	c := &Config{}
+	assert.Equal(webutil.PortFromBindAddr(DefaultBindAddr), c.PortOrDefault())
+	c.BindAddr = ":10"
+	assert.Equal(10, c.PortOrDefault())
+	c.Port = 10
+	assert.Equal(c.Port, c.PortOrDefault())
+}
+
+func TestConfigBaseURLIsSecureScheme(t *testing.T) {
+	assert := assert.New(t)
+	c := &Config{}
+	assert.False(c.BaseURLIsSecureScheme())
+	c.BaseURL = "http://hello.com"
+	assert.False(c.BaseURLIsSecureScheme())
+	c.BaseURL = "https://hello.com"
+	assert.True(c.BaseURLIsSecureScheme())
+}
