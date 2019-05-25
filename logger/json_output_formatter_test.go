ca126f70097c37f6207defb165c15a7d9041542d
Will Charczuk <will@blend.com>
2019-05-25 13:26:26 -0700

    more tests

---
+++ logger/json_output_formatter_test.go
@@ -0,0 +1,24 @@
+package logger
+
+import (
+	"bytes"
+	"context"
+	"testing"
+
+	"github.com/blend/go-sdk/assert"
+)
+
+func TestJSONOutputFormatter(t *testing.T) {
+	assert := assert.New(t)
+
+	jf := NewJSONOutputFormatter(OptJSONPretty())
+	assert.True(jf.Pretty)
+	jf.Pretty = false
+
+	me := NewMessageEvent(Info, "this is a test")
+
+	buf := new(bytes.Buffer)
+	assert.Nil(jf.WriteFormat(context.Background(), buf, me))
+
+	assert.Contains(buf.String(), "\"message\":\"this is a test\"")
+}
