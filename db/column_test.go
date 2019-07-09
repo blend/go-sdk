93e5c3188949e2f7caa5f61bad78af259ca125b8
Will Charczuk <will@blend.com>
2019-07-08 18:29:33 -0700

    updates

--- db/column_test.go
+++ db/column_test.go
@@ -72,9 +72,48 @@ func TestSetValueNil(t *testing.T) {
 	a.NotNil(col)
 
 	var myValue *int
-	err := col.SetValue(&obj, &myValue)
+	err := col.SetValue(&obj, myValue)
 	a.Nil(err)
 	a.Nil(obj.PointerColumn)
+}
+
+func TestSetValueSQLNullable(t *testing.T) {
+	a := assert.New(t)
+
+	obj := myStruct{
+		Unique:        "foo",
+		PointerColumn: ref.Int(1234),
+	}
+	meta := CachedColumnCollectionFromInstance(obj)
+
+	col := meta.Lookup()["unique"]
+	a.NotNil(col)
+
+	myValue := sql.NullString{
+		String: "bar",
+		Valid:  true,
+	}
+	err := col.SetValue(&obj, myValue)
+	a.Nil(err)
+	a.Equal("bar", obj.Unique)
+}
+
+func TestSetValueSQLNullableInvalid(t *testing.T) {
+	a := assert.New(t)
+
+	obj := myStruct{
+		Unique:        "foo",
+		PointerColumn: ref.Int(1234),
+	}
+	meta := CachedColumnCollectionFromInstance(obj)
+
+	col := meta.Lookup()["unique"]
+	a.NotNil(col)
+
+	myValue := sql.NullString{}
+	err := col.SetValue(&obj, myValue)
+	a.Nil(err)
+	a.Equal("", obj.Unique)
 }
 
 func TestGetValue(t *testing.T) {
