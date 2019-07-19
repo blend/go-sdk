2bc54fa3e022b3084d89885435a30aab61eb36b6
Will Charczuk <will@blend.com>
2019-07-19 10:10:22 -0700

    moar string functions

--- validate/string.go
+++ validate/string.go
@@ -1,7 +1,13 @@
 package validate
 
 import (
+	"net/mail"
+	"net/url"
 	"regexp"
+	"strings"
+	"unicode"
+
+	"github.com/blend/go-sdk/uuid"
 
 	"github.com/blend/go-sdk/ex"
 )
@@ -11,6 +17,12 @@ const (
 	ErrStringLengthMin ex.Class = "string should be a minimum length"
 	ErrStringLengthMax ex.Class = "string should be a minimum length"
 	ErrStringMatches   ex.Class = "string should match regular expression"
+	ErrStringIsUpper   ex.Class = "string should be uppercase"
+	ErrStringIsLower   ex.Class = "string should be lowercase"
+	ErrStringIsTitle   ex.Class = "string should be titlecase"
+	ErrStringIsUUID    ex.Class = "string should be a uuid"
+	ErrStringIsEmail   ex.Class = "string should be a valid email address"
+	ErrStringIsURI     ex.Class = "string should be a valid uri"
 )
 
 // String contains helpers for string validation.
@@ -61,4 +73,59 @@ func (s String) Matches(expression string) Validator {
 		}
 		return nil
 	}
+}
+
+// IsUpper returns a validator if a string is all uppercase.
+func (s String) IsUpper() error {
+	runes := []rune(string(s))
+	for _, r := range runes {
+		if !unicode.IsUpper(r) {
+			return Error(ErrStringIsUpper)
+		}
+	}
+	return nil
+}
+
+// IsLower returns a validator if a string is all lowercase.
+func (s String) IsLower() error {
+	runes := []rune(string(s))
+	for _, r := range runes {
+		if !unicode.IsLower(r) {
+			return Error(ErrStringIsLower)
+		}
+	}
+	return nil
+}
+
+// IsTitle returns a validator if a string is titlecase.
+// Titlecase is defined as the output of strings.ToTitle(s).
+func (s String) IsTitle() error {
+	if strings.ToTitle(string(s)) == string(s) {
+		return nil
+	}
+	return Error(ErrStringIsTitle)
+}
+
+// IsUUID returns if a string is a valid uuid.
+func (s String) IsUUID() error {
+	if _, err := uuid.Parse(string(s)); err != nil {
+		return Error(ErrStringIsUUID)
+	}
+	return nil
+}
+
+// IsEmail returns if a string is a valid email address.
+func (s String) IsEmail() error {
+	if _, err := mail.ParseAddress(string(s)); err != nil {
+		return Error(ErrStringIsEmail)
+	}
+	return nil
+}
+
+// IsURI returns if a string is a valid uri.
+func (s String) IsURI() error {
+	if _, err := url.ParseRequestURI(string(s)); err != nil {
+		return Error(ErrStringIsURI)
+	}
+	return nil
 }
--- validate/string_test.go
+++ validate/string_test.go
@@ -1,10 +1,12 @@
 package validate
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/blend/go-sdk/assert"
 	"github.com/blend/go-sdk/ex"
+	"github.com/blend/go-sdk/uuid"
 )
 
 func TestStringMin(t *testing.T) {
@@ -68,4 +70,85 @@ func TestStringMatchesError(t *testing.T) {
 	err = String("a foo").Matches("((")() // this should be an invalid regex "(("
 	assert.NotNil(err)
 	assert.NotEqual(ErrValidation, ex.ErrClass(err))
+}
+
+func TestStringIsUpper(t *testing.T) {
+	assert := assert.New(t)
+
+	var verr error
+	verr = String("FOO").IsUpper()
+	assert.Nil(verr)
+
+	verr = String("FOo").IsUpper()
+	assert.NotNil(verr)
+	assert.Equal(ErrStringIsUpper, ex.ErrInner(verr))
+}
+
+func TestStringIsLower(t *testing.T) {
+	assert := assert.New(t)
+
+	var verr error
+	verr = String("foo").IsLower()
+	assert.Nil(verr)
+
+	verr = String("foO").IsLower()
+	assert.NotNil(verr)
+	assert.Equal(ErrStringIsLower, ex.ErrInner(verr))
+}
+
+func TestStringIsTitle(t *testing.T) {
+	assert := assert.New(t)
+
+	var verr error
+	verr = String(strings.ToTitle("this is a test")).IsTitle()
+	assert.Nil(verr)
+
+	verr = String("this is a test").IsTitle()
+	assert.NotNil(verr)
+	assert.Equal(ErrStringIsTitle, ex.ErrInner(verr))
+}
+
+func TestStringIsUUID(t *testing.T) {
+	assert := assert.New(t)
+
+	var verr error
+	verr = String(uuid.V4().String()).IsUUID()
+	assert.Nil(verr)
+
+	verr = String(uuid.V4().ToFullString()).IsUUID()
+	assert.Nil(verr)
+
+	verr = String("asldkfjaslkfjasdlfa").IsUUID()
+	assert.NotNil(verr)
+	assert.Equal(ErrStringIsUUID, ex.ErrInner(verr))
+}
+
+func TestStringIsEmail(t *testing.T) {
+	assert := assert.New(t)
+
+	var verr error
+	verr = String("foo@bar.com").IsEmail()
+	assert.Nil(verr)
+
+	verr = String("foo@bar").IsEmail()
+	assert.Nil(verr)
+
+	verr = String("foo+foo@bar.com").IsEmail()
+	assert.Nil(verr)
+
+	verr = String("this is a test").IsEmail()
+	assert.NotNil(verr)
+	assert.Equal(ErrStringIsEmail, ex.ErrInner(verr))
+}
+
+func TestStringIsURI(t *testing.T) {
+	assert := assert.New(t)
+
+	var verr error
+	verr = String("https://foo.com").IsURI()
+	assert.Nil(verr)
+
+	verr = String("this is a test").IsURI()
+	assert.NotNil(verr)
+	assert.Equal(ErrStringIsURI, ex.ErrInner(verr))
 }
