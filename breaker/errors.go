8ed4d9bf9b97deb27693271446776a1c326bcf92
Will Charczuk <will@blend.com>
2019-08-10 15:43:03 -0700

    error helpers

--- breaker/errors.go
+++ breaker/errors.go
@@ -8,3 +8,13 @@ var (
 	// ErrOpenState is returned when the CB state is open
 	ErrOpenState ex.Class = "circuit breaker is open"
 )
+
+// ErrIsOpen returns if the error is an ErrOpenState.
+func ErrIsOpen(err error) bool {
+	return ex.Is(err, ErrOpenState)
+}
+
+// ErrIsTooManyRequests returns if the error is an ErrTooManyRequests.
+func ErrIsTooManyRequests(err error) bool {
+	return ex.Is(err, ErrTooManyRequests)
+}
