aa10310ba490fc8f9e0bf322014922dbe722bf4e
Will Charczuk <will@blend.com>
2019-08-15 16:50:01 -0700

    adding back airbrake

---
+++ airbrake/listener.go
@@ -0,0 +1,30 @@
+package airbrake
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/blend/go-sdk/logger"
+)
+
+const (
+	// ListenerAirbrake is the airbrake listener name.
+	ListenerAirbrake = "airbrake"
+)
+
+// AddListeners adds airbrake listeners.
+func AddListeners(log logger.Listenable, cfg Config) {
+	if log == nil || cfg.IsZero() {
+		return
+	}
+	client := MustNew(cfg)
+	listener := logger.NewErrorEventListener(func(_ context.Context, ee *logger.ErrorEvent) {
+		if req, ok := ee.State.(*http.Request); ok {
+			client.NotifyWithRequest(ee.Err, req)
+		} else {
+			client.Notify(ee.Err)
+		}
+	})
+	log.Listen(logger.Error, ListenerAirbrake, listener)
+	log.Listen(logger.Fatal, ListenerAirbrake, listener)
+}
