c385e0400a342b076a6b54612793ebb040408211
Will Charczuk <will@blend.com>
2019-09-13 16:44:55 -0700

    adding bindata

---
+++ bindata/bundle_test.go
@@ -0,0 +1,32 @@
+package bindata
+
+import (
+	"bytes"
+	"testing"
+
+	"go/parser"
+	"go/token"
+
+	"github.com/blend/go-sdk/assert"
+)
+
+func TestBundle(t *testing.T) {
+	assert := assert.New(t)
+
+	buffer := new(bytes.Buffer)
+	bundle := new(Bundle)
+	bundle.PackageName = "bindata"
+	err := bundle.Process(buffer, PathConfig{Path: "./testdata", Recursive: true})
+	assert.Nil(err)
+
+	assert.NotEmpty(buffer.Bytes())
+
+	assert.Contains(buffer.String(), "package bindata")
+	assert.Contains(buffer.String(), "testdata/js/app.js")
+	assert.Contains(buffer.String(), "testdata/css/site.css")
+
+	ast, err := parser.ParseFile(token.NewFileSet(), "bindata.go", buffer.Bytes(), parser.ParseComments|parser.AllErrors)
+	assert.Nil(err)
+	assert.NotNil(ast)
+	assert.Len(ast.Imports, 5)
+}
---
+++ bindata/util.go
@@ -0,0 +1,16 @@
+package bindata
+
+import "bytes"
+
+// sanitize prepares a valid UTF-8 string as a raw string constant.
+// Based on https://code.google.com/p/go/source/browse/godoc/static/makestatic.go?repo=tools
+func sanitize(b []byte) []byte {
+	// Replace ` with `+"`"+`
+	b = bytes.Replace(b, []byte("`"), []byte("`+\"`\"+`"), -1)
+
+	// Replace BOM with `+"\xEF\xBB\xBF"+`
+	// (A BOM is valid UTF-8 but not permitted in Go source files.
+	// I wouldn't bother handling this, but for some insane reason
+	// jquery.js has a BOM somewhere in the middle.)
+	return bytes.Replace(b, []byte("\xEF\xBB\xBF"), []byte("`+\"\\xEF\\xBB\\xBF\"+`"), -1)
+}
