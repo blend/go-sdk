88881e714b2aa8497ef5607d51ad2260ca8c4365
Will Charczuk <will@blend.com>
2019-09-26 10:05:38 -0700

    handles errors actually now

--- ansi/table_test.go
+++ ansi/table_test.go
@@ -2,10 +2,57 @@ package ansi
 
 import (
 	"bytes"
+	"fmt"
 	"testing"
 
 	"github.com/blend/go-sdk/assert"
 )
+
+func TestTable(t *testing.T) {
+	assert := assert.New(t)
+
+	buffer := new(bytes.Buffer)
+
+	err := Table(buffer, []string{"foo", "bar"}, [][]string{{"1", "2"}, {"3", "4"}})
+	assert.Nil(err)
+	assert.NotEmpty(buffer.String())
+}
+
+func TestTableEmptyErr(t *testing.T) {
+	assert := assert.New(t)
+
+	buffer := new(bytes.Buffer)
+
+	err := Table(buffer, nil, [][]string{{"1", "2"}, {"3", "4"}})
+	assert.NotNil(err)
+}
+
+func TestTableWriteErr(t *testing.T) {
+	assert := assert.New(t)
+
+	failAfter := &failAfter{MaxBytes: 32}
+
+	err := Table(failAfter, []string{"foo", "bar", "baz"}, [][]string{
+		{"1", "2", "3"},
+		{"4", "5", "6"},
+		{"7", "8", "9"},
+	})
+	assert.NotNil(err)
+	assert.Equal("did fail", err.Error())
+}
+
+type failAfter struct {
+	Written  []byte
+	MaxBytes int
+}
+
+func (fa *failAfter) Write(contents []byte) (int, error) {
+	fa.Written = append(fa.Written, contents...)
+	if len(fa.Written) > fa.MaxBytes {
+		return len(contents), fmt.Errorf("did fail")
+	}
+	return len(contents), nil
+}
 
 func TestTableForSlice(t *testing.T) {
 	assert := assert.New(t)
