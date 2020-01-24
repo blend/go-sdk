502dde7ee15214e5442f87abac90513e75061541
Will Charczuk <will@blend.com>
2020-01-23 19:43:51 -0800

    too much of a good thing

--- graceful/shutdown_options.go
+++ graceful/shutdown_options.go
@@ -2,7 +2,6 @@ package graceful
 
 import (
 	"os"
-	"time"
 )
 
 // OptDefaultShutdownSignal returns an option that sets the shutdown signal to the defaults.
@@ -15,16 +14,10 @@ func OptShutdownSignal(signal chan os.Signal) ShutdownOption {
 	return func(so *ShutdownOptions) { so.ShutdownSignal = signal }
 }
 
-// OptShutdownGracePeriod sets a shutdown grace period.
-func OptShutdownGracePeriod(gracePeriod time.Duration) ShutdownOption {
-	return func(so *ShutdownOptions) { so.ShutdownGracePeriod = gracePeriod }
-}
-
 // ShutdownOption is a mutator for shutdown options.
 type ShutdownOption func(*ShutdownOptions)
 
 // ShutdownOptions are the options for graceful shutdown.
 type ShutdownOptions struct {
-	ShutdownGracePeriod time.Duration
-	ShutdownSignal      chan os.Signal
+	ShutdownSignal chan os.Signal
 }
