9e5230905e8cbd4053ccf51a323a59680efac00e
Danny Hermes <dhermes@blend.com>
2020-07-28 23:20:27 -0500

    Adding test for `OptFormatClientIdentity()`.

--- envoyutil/client_identity_processor_test.go
+++ envoyutil/client_identity_processor_test.go
@@ -5,6 +5,8 @@ import (
 
 	sdkAssert "github.com/blend/go-sdk/assert"
 	"github.com/blend/go-sdk/collections"
+	"github.com/blend/go-sdk/spiffeutil"
+	"github.com/blend/go-sdk/uuid"
 
 	"github.com/blend/go-sdk/envoyutil"
 )
@@ -67,4 +69,24 @@ func TestOptDeniedClientIdentities(t *testing.T) {
 		DeniedClientIdentities: collections.NewSetOfString("x.invalid", "y.invalid", "z.invalid"),
 	}
 	assert.Equal(expected, cip)
+}
+
+func TestOptFormatClientIdentity(t *testing.T) {
+	assert := sdkAssert.New(t)
+
+	cip := &envoyutil.ClientIdentityProcessor{
+		FormatClientIdentity: envoyutil.KubernetesClientIdentityFormatter,
+	}
+	sentinel := uuid.V4().ToFullString()
+	var fn envoyutil.ClientIdentityFormatter = func(_ envoyutil.XFCCElement, _ *spiffeutil.ParsedURI) (string, error) {
+		return sentinel, nil
+	}
+	opt := envoyutil.OptFormatClientIdentity(fn)
+	opt(cip)
+
+	// Can't compare functions for equality, see https://github.com/blend/go-sdk/issues/167
+	// so we make sure our function is as expected.
+	s, err := cip.FormatClientIdentity(envoyutil.XFCCElement{}, nil)
+	assert.Equal(sentinel, s)
+	assert.Nil(err)
 }
