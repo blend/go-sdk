18239d285a45efbd6c7b2d5fa8697752aeb823f6
Danny Hermes <dhermes@blend.com>
2021-01-06 15:09:22 -0600

    Updating `configutil@1.20210106.1`.

--- configutil/int32_source_test.go
+++ configutil/int32_source_test.go
@@ -7,16 +7,16 @@ import (
 	"github.com/blend/go-sdk/assert"
 )
 
-func TestInt646464646464(t *testing.T) {
+func TestInt32(t *testing.T) {
 	assert := assert.New(t)
 
-	intValue := Int64(0)
-	ptr, err := intValue.Int64(context.TODO())
+	intValue := Int32(0)
+	ptr, err := intValue.Int32(context.TODO())
 	assert.Nil(ptr)
 	assert.Nil(err)
 
-	intValue = Int64(1234)
-	ptr, err = intValue.Int64(context.TODO())
+	intValue = Int32(1234)
+	ptr, err = intValue.Int32(context.TODO())
 	assert.Nil(err)
 	assert.NotNil(ptr)
 	assert.Equal(1234, *ptr)
--- configutil/option.go
+++ configutil/option.go
@@ -1,6 +1,7 @@
 package configutil
 
 import (
+	"bytes"
 	"context"
 	"io"
 
@@ -26,35 +27,71 @@ func OptContext(ctx context.Context) Option {
 	}
 }
 
-// OptContents sets the contents on the options.
-func OptContents(ext string, contents io.Reader) Option {
+// OptContents sets config contents on the options.
+func OptContents(contents []ConfigContents) Option {
 	return func(co *ConfigOptions) error {
-		co.ContentsExt = ext
 		co.Contents = contents
 		return nil
 	}
 }
 
-// OptAddFilePaths adds paths to search for the config file.
-func OptAddFilePaths(paths ...string) Option {
+// OptAddContent adds contents to the options as a reader.
+func OptAddContent(ext string, content io.Reader) Option {
+	return func(co *ConfigOptions) error {
+		co.Contents = append(co.Contents, ConfigContents{
+			Ext:      ext,
+			Contents: content,
+		})
+		return nil
+	}
+}
+
+// OptAddContentString adds contents to the options as a string.
+func OptAddContentString(ext string, contents string) Option {
+	return func(co *ConfigOptions) error {
+		co.Contents = append(co.Contents, ConfigContents{
+			Ext:      ext,
+			Contents: bytes.NewReader([]byte(contents)),
+		})
+		return nil
+	}
+}
+
+// OptAddPaths adds paths to search for the config file.
+//
+// These paths will be added after the default paths.
+func OptAddPaths(paths ...string) Option {
 	return func(co *ConfigOptions) error {
 		co.FilePaths = append(co.FilePaths, paths...)
 		return nil
 	}
 }
 
-// OptAddPreferredFilePaths adds paths to search first for the config file.
-func OptAddPreferredFilePaths(paths ...string) Option {
+// OptAddFilePaths is deprecated; use `OptAddPaths`
+func OptAddFilePaths(paths ...string) Option {
+	return OptAddPaths(paths...)
+}
+
+// OptAddPreferredPaths adds paths to search first for the config file.
+func OptAddPreferredPaths(paths ...string) Option {
 	return func(co *ConfigOptions) error {
 		co.FilePaths = append(paths, co.FilePaths...)
 		return nil
 	}
 }
 
-// OptFilePaths sets paths to search for the config file.
-func OptFilePaths(paths ...string) Option {
+// OptPaths sets paths to search for the config file.
+func OptPaths(paths ...string) Option {
 	return func(co *ConfigOptions) error {
 		co.FilePaths = paths
+		return nil
+	}
+}
+
+// OptUnsetPaths removes default paths from the paths set.
+func OptUnsetPaths() Option {
+	return func(co *ConfigOptions) error {
+		co.FilePaths = nil
 		return nil
 	}
 }
