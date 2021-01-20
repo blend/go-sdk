c70abb8ed6322a7d27ed83acaeac2a5be98ec211
Danny Hermes <dhermes@blend.com>
2021-01-20 16:48:12 -0600

    Updating `validate@1.20210106.1`.

--- validate/errorf.go
+++ validate/errorf.go
@@ -6,18 +6,18 @@ import (
 	"github.com/blend/go-sdk/ex"
 )
 
-// Error returns a new validation error.
+// Errorf returns a new validation error.
 // The root class of the error will be ErrValidation.
 // The root stack will begin the frame above this call to error.
 // The inner error will the cause of the validation vault.
-func Error(cause error, value interface{}, messageArgs ...interface{}) error {
+func Errorf(cause error, value interface{}, format string, args ...interface{}) error {
 	return &ex.Ex{
-		Class:      ErrValidation,
-		StackTrace: ex.Callers(ex.DefaultNewStartDepth + 1),
+		Class: ErrValidation,
 		Inner: &ValidationError{
 			Cause:   cause,
 			Value:   value,
-			Message: fmt.Sprint(messageArgs...),
+			Message: fmt.Sprintf(format, args...),
 		},
+		StackTrace: ex.Callers(ex.DefaultNewStartDepth + 1),
 	}
 }
